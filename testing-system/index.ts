886d8d0ae3741759c366a7fd99a6c32e8c080027
ngfam <ngfamgithub@gmail.com>
2022-03-27 18:57:35 +0700

    Restructure testing environment

--- testing-system/index.ts
+++ testing-system/index.ts
@@ -2,11 +2,10 @@ import { buildEnv, TestEnv, Mode } from './environment';
 
 async function main() {
   const env: TestEnv = await buildEnv();
-  
+
   console.log(await env.qiLyt.indexCurrent());
   await env.qiLyt.addFakeIncome(env);
   console.log(await env.qiLyt.indexCurrent());
-
 }
 
 main();
