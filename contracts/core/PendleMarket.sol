f2d059f64e9f0accc3bfd7bf326777b1cfbb29e7
UncleGrandpa925 <vuonglongtoan@gmail.com>
2022-04-17 16:14:33 +0800

    Overhaul YieldToken with new redeem functions

--- contracts/core/PendleMarket.sol
+++ contracts/core/PendleMarket.sol
@@ -193,10 +193,7 @@ contract PendleMarket is PendleBaseToken, IPMarket, ReentrancyGuard {
     /// @dev this function is just a place holder. Later on the rewards will be transferred to the liquidity minining
     /// instead
     function redeemScyReward() external returns (uint256[] memory outAmounts) {
-        outAmounts = ISuperComposableYield(SCY).redeemReward(
-            address(this),
-            IPMarketFactory(factory).treasury()
-        );
+        outAmounts = ISuperComposableYield(SCY).redeemReward(address(this));
     }
 
     /// the only non-view part in this function is the ISuperComposableYield(SCY).scyIndexCurrent()
