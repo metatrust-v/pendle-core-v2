9f0ffa2122f28c1ba71748fb52c5a5de2ea27378
ngfam <ngfamgithub@gmail.com>
2022-04-20 20:57:55 +0700

    Add reward caching per block for RewardManager & YT

--- contracts/core/PendleYieldToken.sol
+++ contracts/core/PendleYieldToken.sol
@@ -151,6 +151,7 @@ contract PendleYieldToken is PendleBaseToken, RewardManager, IPYieldToken {
         virtual
         override
     {
+        if (!_shouldUpdateGlobalReward()) return;
         _redeemExternalReward();
 
         _initGlobalReward(rewardTokens);
