89eb4339c91782c09c3462922b3fc9107304e0ba
UncleGrandpa925 <vuonglongtoan@gmail.com>
2022-06-24 19:04:00 +0700

    [MF] Make exchangeRate of SCY a view function

--- contracts/SuperComposableYield/SCY-implementations/PendleERC4626SCY.sol
+++ contracts/SuperComposableYield/SCY-implementations/PendleERC4626SCY.sol
@@ -13,8 +13,6 @@ contract PendleERC4626SCY is SCYBase {
 
     address public immutable underlying;
 
-    uint256 public override exchangeRateStored;
-
     constructor(
         string memory _name,
         string memory _symbol,
@@ -26,7 +24,7 @@ contract PendleERC4626SCY is SCYBase {
     }
 
     function _validateERC4626SCY() internal {
-        require(exchangeRateCurrent() <= 1e30, "too big exchangeRate");
+        require(exchangeRate() <= 1e30, "too big exchangeRate");
     }
 
     /*///////////////////////////////////////////////////////////////
@@ -69,12 +67,8 @@ contract PendleERC4626SCY is SCYBase {
                                SCY-INDEX
     //////////////////////////////////////////////////////////////*/
 
-    function exchangeRateCurrent() public virtual override returns (uint256 currentRate) {
-        currentRate = IERC4626(yieldToken).convertToAssets(Math.ONE);
-
-        emit ExchangeRateUpdated(exchangeRateStored, currentRate);
-
-        exchangeRateStored = currentRate;
+    function exchangeRate() public view virtual override returns (uint256) {
+        return IERC4626(yieldToken).convertToAssets(Math.ONE);
     }
 
     /*///////////////////////////////////////////////////////////////
