33cb1992f65f54d955167e25e0e084b5dc206ef0
ngfam <ngfamgithub@gmail.com>
2022-08-08 13:56:50 +0700

    [FB] Fix initialization bug in sJOE constructor

--- contracts/SuperComposableYield/SCY-implementations/TraderJoe/PendleSJoeSCY.sol
+++ contracts/SuperComposableYield/SCY-implementations/TraderJoe/PendleSJoeSCY.sol
@@ -140,8 +140,7 @@ contract PendleSJoeSCY is SCYBaseWithRewards {
         uint256 sJoeDepositFeePrecision = ISJoe(SJOE).depositFeePercent();
 
         amountSharesOut =
-            (((1e18 - sJoeDepositFeePrecision / 1e18) * amountTokenToDeposit) * 1e18) /
-            exchangeRate();
+            ((1e18 - sJoeDepositFeePrecision / 1e18) * amountTokenToDeposit);
     }
 
     function _previewRedeem(address, uint256 amountSharesToRedeem)
