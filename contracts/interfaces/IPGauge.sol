a13d1ff7fba34ccd400fda85d2261e091fc6cb04
ngfam <ngfamgithub@gmail.com>
2022-08-22 12:03:33 +0700

    [SF] IPGauge inherits IRewardManager

--- contracts/interfaces/IPGauge.sol
+++ contracts/interfaces/IPGauge.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.15;
 
-interface IPGauge {
+import "./IRewardManager.sol";
+
+interface IPGauge is IRewardManager {
     function totalActiveSupply() external view returns (uint256);
+
+    function activeBalance(address user) external view returns (uint256);
 }
