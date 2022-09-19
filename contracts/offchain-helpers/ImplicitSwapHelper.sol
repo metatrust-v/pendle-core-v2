7aae6e423f93c28ba768130c220ace265ded0b01
ngfam <ngfamgithub@gmail.com>
2022-09-19 09:59:07 +0700

    [SF] Draft implicit swap lib

---
+++ contracts/offchain-helpers/ImplicitSwapHelper.sol
@@ -0,0 +1,297 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.15;
+
+import "../libraries/math/Math.sol";
+import "../libraries/math/LogExpMath.sol";
+import "../libraries/helpers/PYIndex.sol";
+import "../libraries/helpers/MiniHelpers.sol";
+
+struct MarketState {
+    int256 totalPt;
+    int256 totalScy;
+    int256 totalLp;
+    address treasury;
+    /// immutable variables ///
+    int256 scalarRoot;
+    uint256 lnFeeRateRoot;
+    uint256 expiry;
+    uint256 reserveFeePercent; // base 100
+    /// last trade data ///
+    uint256 lastLnImpliedRate;
+}
+
+// params that are expensive to compute, therefore we pre-compute them
+struct MarketPreCompute {
+    int256 rateScalar;
+    int256 totalAsset;
+    int256 rateAnchor;
+    int256 feeRate;
+}
+
+// solhint-disable ordering
+library MarketMathCore {
+    using Math for uint256;
+    using Math for int256;
+    using LogExpMath for int256;
+    using PYIndexLib for PYIndex;
+
+    int256 internal constant MINIMUM_LIQUIDITY = 10**3;
+    int256 internal constant PERCENTAGE_DECIMALS = 100;
+    uint256 internal constant DAY = 86400;
+    uint256 internal constant IMPLIED_RATE_TIME = 365 * DAY;
+
+    int256 internal constant MAX_MARKET_PROPORTION = (1e18 * 96) / 100;
+
+    using Math for uint256;
+    using Math for int256;
+
+    /*///////////////////////////////////////////////////////////////
+                    CORE FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    function executeTradeCore(
+        MarketState memory market,
+        PYIndex index,
+        int256 netPtToAccount,
+        uint256 blockTime
+    ) internal pure returns (int256 netScyToAccount, int256 netScyToReserve) {
+        /// ------------------------------------------------------------
+        /// CHECKS
+        /// ------------------------------------------------------------
+        require(!MiniHelpers.isExpired(market.expiry, blockTime), "market expired");
+        require(market.totalPt > netPtToAccount, "insufficient liquidity");
+
+        /// ------------------------------------------------------------
+        /// MATH
+        /// ------------------------------------------------------------
+        MarketPreCompute memory comp = getMarketPreCompute(market, index, blockTime);
+
+        (int256 netAssetToAccount, int256 netAssetToReserve) = calcTrade(
+            market,
+            comp,
+            netPtToAccount
+        );
+
+        netScyToAccount = netAssetToAccount < 0
+            ? index.assetToScyUp(netAssetToAccount)
+            : index.assetToScy(netAssetToAccount);
+        netScyToReserve = index.assetToScy(netAssetToReserve);
+
+        /// ------------------------------------------------------------
+        /// WRITE
+        /// ------------------------------------------------------------
+        _setNewMarketStateTrade(
+            market,
+            comp,
+            index,
+            netPtToAccount,
+            netScyToAccount,
+            netScyToReserve,
+            blockTime
+        );
+    }
+
+    function getMarketPreCompute(
+        MarketState memory market,
+        PYIndex index,
+        uint256 blockTime
+    ) internal pure returns (MarketPreCompute memory res) {
+        require(!MiniHelpers.isExpired(market.expiry, blockTime), "market expired");
+
+        uint256 timeToExpiry = market.expiry - blockTime;
+
+        res.rateScalar = _getRateScalar(market, timeToExpiry);
+        res.totalAsset = index.scyToAsset(market.totalScy);
+
+        require(market.totalPt != 0 && res.totalAsset != 0, "invalid market state");
+
+        res.rateAnchor = _getRateAnchor(
+            market.totalPt,
+            market.lastLnImpliedRate,
+            res.totalAsset,
+            res.rateScalar,
+            timeToExpiry
+        );
+        res.feeRate = _getExchangeRateFromImpliedRate(market.lnFeeRateRoot, timeToExpiry);
+    }
+
+    function calcTrade(
+        MarketState memory market,
+        MarketPreCompute memory comp,
+        int256 netPtToAccount
+    ) internal pure returns (int256 netAssetToAccount, int256 netAssetToReserve) {
+        int256 preFeeExchangeRate = _getExchangeRate(
+            market.totalPt,
+            comp.totalAsset,
+            comp.rateScalar,
+            comp.rateAnchor,
+            netPtToAccount
+        );
+
+        int256 preFeeAssetToAccount = netPtToAccount.divDown(preFeeExchangeRate).neg();
+        int256 fee = comp.feeRate;
+
+        if (netPtToAccount > 0) {
+            fee = preFeeAssetToAccount.mulDown(Math.IONE - fee);
+        } else {
+            fee = ((preFeeAssetToAccount * (Math.IONE - fee)) / fee).neg();
+        }
+
+        netAssetToReserve = (fee * market.reserveFeePercent.Int()) / PERCENTAGE_DECIMALS;
+        netAssetToAccount = preFeeAssetToAccount - fee;
+    }
+
+    function _setNewMarketStateTrade(
+        MarketState memory market,
+        MarketPreCompute memory comp,
+        PYIndex index,
+        int256 netPtToAccount,
+        int256 netScyToAccount,
+        int256 netScyToReserve,
+        uint256 blockTime
+    ) internal pure {
+        uint256 timeToExpiry = market.expiry - blockTime;
+
+        market.totalPt = market.totalPt.subNoNeg(netPtToAccount);
+        market.totalScy = market.totalScy.subNoNeg(netScyToAccount + netScyToReserve);
+
+        market.lastLnImpliedRate = _getLnImpliedRate(
+            market.totalPt,
+            index.scyToAsset(market.totalScy),
+            comp.rateScalar,
+            comp.rateAnchor,
+            timeToExpiry
+        );
+        require(market.lastLnImpliedRate != 0, "zero lnImpliedRate");
+    }
+
+    function _getRateAnchor(
+        int256 totalPt,
+        uint256 lastLnImpliedRate,
+        int256 totalAsset,
+        int256 rateScalar,
+        uint256 timeToExpiry
+    ) internal pure returns (int256 rateAnchor) {
+        int256 newExchangeRate = _getExchangeRateFromImpliedRate(lastLnImpliedRate, timeToExpiry);
+        {
+            int256 proportion = totalPt.divDown(totalPt + totalAsset);
+
+            int256 lnProportion = _logProportion(proportion);
+
+            rateAnchor = newExchangeRate - lnProportion.divDown(rateScalar);
+        }
+    }
+
+    /// @notice Calculates the current market implied rate.
+    /// @return lnImpliedRate the implied rate
+    function _getLnImpliedRate(
+        int256 totalPt,
+        int256 totalAsset,
+        int256 rateScalar,
+        int256 rateAnchor,
+        uint256 timeToExpiry
+    ) internal pure returns (uint256 lnImpliedRate) {
+        // This will check for exchange rates < Math.IONE
+        int256 exchangeRate = _getExchangeRate(totalPt, totalAsset, rateScalar, rateAnchor, 0);
+
+        // exchangeRate >= 1 so its ln >= 0
+        uint256 lnRate = exchangeRate.ln().Uint();
+
+        lnImpliedRate = (lnRate * IMPLIED_RATE_TIME) / timeToExpiry;
+    }
+
+    /// @notice Converts an implied rate to an exchange rate given a time to expiry. The
+    /// formula is E = e^rt
+    function _getExchangeRateFromImpliedRate(uint256 lnImpliedRate, uint256 timeToExpiry)
+        internal
+        pure
+        returns (int256 exchangeRate)
+    {
+        uint256 rt = (lnImpliedRate * timeToExpiry) / IMPLIED_RATE_TIME;
+
+        exchangeRate = LogExpMath.exp(rt.Int());
+    }
+
+    function _getExchangeRate(
+        int256 totalPt,
+        int256 totalAsset,
+        int256 rateScalar,
+        int256 rateAnchor,
+        int256 netPtToAccount
+    ) internal pure returns (int256 exchangeRate) {
+        int256 numerator = totalPt.subNoNeg(netPtToAccount);
+
+        int256 proportion = (numerator.divDown(totalPt + totalAsset));
+
+        int256 lnProportion = _logProportion(proportion);
+
+        exchangeRate = lnProportion.divDown(rateScalar) + rateAnchor;
+    }
+
+    function _logProportion(int256 proportion) internal pure returns (int256 res) {
+        int256 logitP = proportion.divDown(Math.IONE - proportion);
+
+        res = logitP.ln();
+    }
+
+    function _getRateScalar(MarketState memory market, uint256 timeToExpiry)
+        internal
+        pure
+        returns (int256 rateScalar)
+    {
+        rateScalar = (market.scalarRoot * IMPLIED_RATE_TIME.Int()) / timeToExpiry.Int();
+        require(rateScalar > 0, "rateScalar underflow");
+    }
+
+    function setInitialLnImpliedRate(
+        MarketState memory market,
+        PYIndex index,
+        int256 initialAnchor,
+        uint256 blockTime
+    ) internal pure {
+        /// ------------------------------------------------------------
+        /// CHECKS
+        /// ------------------------------------------------------------
+        require(blockTime < market.expiry, "market expired");
+
+        /// ------------------------------------------------------------
+        /// MATH
+        /// ------------------------------------------------------------
+        int256 totalAsset = index.scyToAsset(market.totalScy);
+        uint256 timeToExpiry = market.expiry - blockTime;
+        int256 rateScalar = _getRateScalar(market, timeToExpiry);
+
+        /// ------------------------------------------------------------
+        /// WRITE
+        /// ------------------------------------------------------------
+        market.lastLnImpliedRate = _getLnImpliedRate(
+            market.totalPt,
+            totalAsset,
+            rateScalar,
+            initialAnchor,
+            timeToExpiry
+        );
+    }
+}
+
+contract ImplicitSwapHelper {
+    using MarketMathCore for MarketState;
+
+    int256 public constant K = 100;
+
+    function simulateTrade(
+        MarketState memory state,
+        int256 netPtToAccount,
+        uint256 exchangeRate
+    ) external view returns (int256 netScyToAccount) {
+        PYIndex index = PYIndex.wrap(exchangeRate);
+        for (int256 i = 0; i < K; ++i) {
+            (int256 scyToAccount, ) = state.executeTradeCore(
+                index,
+                netPtToAccount / K,
+                block.timestamp
+            );
+            netScyToAccount += scyToAccount;
+        }
+    }
+}
